dc4562bb0dd1e60f2c5566f3d45a2148629500f4
Maximilian Betz <Maximilian.Betz@awi.de>
2021-01-09 12:34:41 +0100

    initial creation of two examples of a simple lora receiver and sender.

---
+++ SimpleLoraReceiver/LoRaReceiver/LoRaReceiver.ino
@@ -0,0 +1,45 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+const long frequency = 868E6;  // LoRa Frequency
+
+const int csPin = 5;          // LoRa radio chip select
+const int resetPin = 15;        // LoRa radio reset
+const int irqPin = 4;          // change for your board; must be a hardware interrupt pin
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+
+  LoRa.setPins(csPin, resetPin, irqPin);
+  
+  if (!LoRa.begin(frequency)) {
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);
+  }
+
+  Serial.println("LoRa init succeeded.");
+
+
+  /*Dump Register to check default config*/
+  LoRa.dumpRegisters(Serial);
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
