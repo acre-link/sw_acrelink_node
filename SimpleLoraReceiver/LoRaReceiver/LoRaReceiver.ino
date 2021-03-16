b2b5a5e4e0a98d5f2c53f231fe7a2acac9c8a294
Maximilian Betz <Maximilian.Betz@awi.de>
2021-03-16 17:15:12 +0100

    working version.

--- SimpleLoraReceiver/LoRaReceiver/LoRaReceiver.ino
+++ SimpleLoraReceiver/LoRaReceiver/LoRaReceiver.ino
@@ -8,17 +8,26 @@ const int resetPin = 15;        // LoRa radio reset
 const int irqPin = 4;          // change for your board; must be a hardware interrupt pin
 const int spreadingFactor = 10;
 
-bool txDoneFlag = false;
-bool rxDoneFlag = false;
+volatile bool txDoneFlag = false;
+volatile bool rxDoneFlag = false;
+volatile int rxPacketSize = 0;
+volatile uint8_t rxMessageBuffer[30];
+volatile int8_t rxRssi = 0;
 
 /* Set flag if reception occured*/
-void onReceive(int packetSize) 
+void onReceiveDone(int packetSize) 
 {
+  for(int i = 0; i<packetSize; i++)
+  {
+    rxMessageBuffer[i] = LoRa.read();
+  }
+  rxRssi = LoRa.packetRssi();
+  rxPacketSize = packetSize;
   rxDoneFlag = true;
 }
 
 /* Set flag once transmission done*/
-void onTxDone() 
+void onTransmittDone() 
 {
   txDoneFlag = true;
 }
@@ -28,7 +37,8 @@ void setup() {
   while (!Serial);
   Serial.println("LoRa Receiver");
 
-
+  
+  //pinMode(irqPin, INPUT);   //TODO: test if this helps
 
 
   LoRa.setPins(csPin, resetPin, irqPin);
@@ -48,16 +58,41 @@ void setup() {
   /*Dump Register to check default config*/
   //LoRa.dumpRegisters(Serial);
 
-  LoRa.onReceive(onReceive);
-  //LoRa.onTxDone(onTxDone);
+  LoRa.onReceive(onReceiveDone);
+  //LoRa.enableInvertIQ();
+  
+  //LoRa.onTxDone(onTransmittDone);
 
-  //LoRa.receive();
+  LoRa.receive();
+  
+
 }
 
 void loop() {
 
 
+  if(rxDoneFlag == true)
+  {
+    Serial.print("Received something. Length: ");
+    Serial.print(rxPacketSize, DEC);
+    Serial.print(" Payload: "); 
+
+    for(int i = 0; i < rxPacketSize; i++)
+    {
+      Serial.print(rxMessageBuffer[i], HEX);
+      Serial.print(":");
+    }
+    Serial.print(" RSSI: ");
+    Serial.println(rxRssi, DEC); 
+
+    
+    
+    rxDoneFlag = false;
+
+    /*Decode Message and Push here.*/
+  }
   
+  /*
   // try to parse packet
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
@@ -76,5 +111,5 @@ void loop() {
     Serial.print("' with RSSI ");
     Serial.println(LoRa.packetRssi());
   }
-
+  */
 }
