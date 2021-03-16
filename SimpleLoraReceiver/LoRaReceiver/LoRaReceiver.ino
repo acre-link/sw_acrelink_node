314ee2184db9a6c7c276cbbc96969ba1362fe19c
Maximilian Betz <Maximilian.Betz@awi.de>
2021-03-16 19:01:24 +0100

    rx working but deconding / forwarding not done yet.

--- SimpleLoraReceiver/LoRaReceiver/LoRaReceiver.ino
+++ SimpleLoraReceiver/LoRaReceiver/LoRaReceiver.ino
@@ -14,9 +14,55 @@ volatile int rxPacketSize = 0;
 volatile uint8_t rxMessageBuffer[30];
 volatile int8_t rxRssi = 0;
 
+#define LORA_MAX_LENGTH 100
+#define LORA_NUM_OF_MESSAGES 3
+struct LORA_RX_MESSAGE 
+{
+  volatile uint8_t rx_rssi; 
+  volatile uint8_t rx_length;
+  volatile uint8_t rx_data[LORA_MAX_LENGTH];
+};
+volatile uint8_t rxMessageIndex = 0;
+volatile LORA_RX_MESSAGE rxMessages[LORA_NUM_OF_MESSAGES];
+
+
+
 /* Set flag if reception occured*/
 void onReceiveDone(int packetSize) 
 {
+
+/*  somehow not working!
+if (rxMessageIndex < LORA_NUM_OF_MESSAGES - 1)
+{
+  for(int i = 0; i < packetSize; i++)
+  {
+    rxMessages[rxMessageIndex].rx_data[i] = LoRa.read();
+    rxMessageBuffer[i] = rxMessages[rxMessageIndex].rx_data[i];
+  }
+  rxMessages[rxMessageIndex].rx_length = packetSize;
+  rxMessages[rxMessageIndex].rx_rssi = LoRa.packetRssi();
+
+  rxMessageIndex++;
+}
+*/
+
+if( false == rxDoneFlag )
+{
+   for(int i = 0; i < packetSize; i++)
+  {
+    rxMessageBuffer[i] = LoRa.read();
+  }
+  rxPacketSize = packetSize;
+  rxRssi = LoRa.packetRssi();
+
+  rxDoneFlag = true;
+}
+
+else
+{
+  /*Message buffer overrun. Can not handle new message*/
+}
+  /*
   for(int i = 0; i<packetSize; i++)
   {
     rxMessageBuffer[i] = LoRa.read();
@@ -24,6 +70,7 @@ void onReceiveDone(int packetSize)
   rxRssi = LoRa.packetRssi();
   rxPacketSize = packetSize;
   rxDoneFlag = true;
+  */
 }
 
 /* Set flag once transmission done*/
@@ -37,10 +84,7 @@ void setup() {
   while (!Serial);
   Serial.println("LoRa Receiver");
 
-  
-  //pinMode(irqPin, INPUT);   //TODO: test if this helps
-
-
+ 
   LoRa.setPins(csPin, resetPin, irqPin);
   
   if (!LoRa.begin(frequency)) {
@@ -59,57 +103,65 @@ void setup() {
   //LoRa.dumpRegisters(Serial);
 
   LoRa.onReceive(onReceiveDone);
-  //LoRa.enableInvertIQ();
+
   
   //LoRa.onTxDone(onTransmittDone);
-
   LoRa.receive();
-  
-
 }
+
+struct DATA
+{
+   String device_id;
+   uint8_t rssi;
+   String recorded_at;
+   uint16_t values[];  //Will be transmitted as HEX String to JSON
+};
+
+DATA abe;
 
 void loop() {
 
+//Disable and Enable Interrupts
+//noInterrupts();
+//interrupts();
 
+/*
+//LoRa.onReceive(NULL); // detatch LoRa RX Done interrupt again
+while(rxMessageIndex > 0)
+{
+  Serial.print("Received something. Length: ");
+  Serial.print(rxMessages[rxMessageIndex].rx_length, DEC);
+  int i = 0;
+  for(i = 0; i < rxMessages[rxMessageIndex].rx_length - 1; i++)
+  {
+    Serial.print(rxMessages[rxMessageIndex].rx_data[i], HEX);
+    Serial.print(":");
+  }
+  Serial.print(rxMessages[rxMessageIndex].rx_data[i], HEX);
+  Serial.print(" RSSI: ");
+  Serial.println(rxMessages[rxMessageIndex].rx_rssi, DEC); 
+
+  
+  rxMessageIndex--;
+}
+//LoRa.onReceive(onReceiveDone); //enable LoRa RX Done interrupt again
+*/
   if(rxDoneFlag == true)
   {
     Serial.print("Received something. Length: ");
     Serial.print(rxPacketSize, DEC);
     Serial.print(" Payload: "); 
-
-    for(int i = 0; i < rxPacketSize; i++)
+    int i = 0;
+    for(i = 0; i < rxPacketSize - 1; i++)
     {
       Serial.print(rxMessageBuffer[i], HEX);
       Serial.print(":");
     }
+    Serial.print(rxMessageBuffer[i], HEX);
+    
     Serial.print(" RSSI: ");
     Serial.println(rxRssi, DEC); 
 
-    
-    
     rxDoneFlag = false;
-
-    /*Decode Message and Push here.*/
   }
-  
-  /*
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    // received a packet
-    Serial.print("Received packet. Length: ");
-    Serial.print(packetSize, DEC);
-    Serial.print(" :");
-
-    // read packet
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read(),HEX);
-       Serial.print(":");
-    }
-
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-  }
-  */
 }
